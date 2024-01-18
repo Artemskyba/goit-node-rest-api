e4fb27b21408bd0cea31b4658cd7974c273178ef
Artemskyba <artemgennadievich@icloud.com>
2024-01-18 21:52:45 +0200

    first commit

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,39 @@
-import contactsService from "../services/contactsServices.js";
+const contactsService = require("../services/contactsServices");
+const handleNotFound = (res) => {
+  res.status(404).json({ message: "Not found" });
+};
+const getAllContacts = async (req, res) => {
+  const result = await contactsService.listContacts();
+  res.status(200).json(result);
+};
 
-export const getAllContacts = (req, res) => {};
+const getContactById = async (req, res) => {
+  const result = await contactsService.getContactById(req.params.id);
+  if (result) {
+    res.status(200).json(result);
+  } else {
+    handleNotFound(res);
+  }
+};
 
-export const getContactById = (req, res) => {};
+const deleteContact = async (req, res) => {
+  const result = await contactsService.removeContact(req.params.id);
+  if (result) {
+    res.status(200).json(result);
+  } else {
+    handleNotFound(res);
+  }
+};
 
-export const deleteContact = (req, res) => {};
+const createContact = (req, res) => {};
 
-export const createContact = (req, res) => {};
+const updateContact = (req, res) => {};
 
-export const updateContact = (req, res) => {};
+module.exports = {
+  getAllContacts,
+  getContactById,
+  deleteContact,
+  createContact,
+  updateContact,
+};
+// add try catch!!!!
---
+++ services/contactsServices.js
@@ -0,0 +1,55 @@
+const { v4: uuidv4 } = require("uuid");
+const fs = require("fs").promises;
+const path = require("path");
+
+const contactsPath = path.join(__dirname, "..", "db", "contacts.json");
+
+async function listContacts() {
+  const contacts = JSON.parse(await fs.readFile(contactsPath, "utf-8"));
+  return contacts;
+}
+
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const data = contacts.find((contact) => contact.id === contactId);
+  if (data) {
+    return data;
+  }
+  return null;
+}
+
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+
+  if (index !== -1) {
+    const deletedContact = contacts.splice(index, 1)[0];
+    await fs.writeFile(
+      contactsPath,
+      JSON.stringify(contacts, null, 2),
+      "utf-8"
+    );
+    return deletedContact;
+  }
+  return null;
+}
+
+async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+  const newContact = {
+    id: uuidv4(),
+    name: name,
+    email: email,
+    phone: phone,
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2), "utf-8");
+  return newContact;
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
